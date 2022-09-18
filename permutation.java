065aeba93f5de49cfb3975994826adaf8f46b3d8
kalikiri-bharath06 <112929839+kalikiri-bharath06@users.noreply.github.com>
2022-09-18 18:23:08 +0530

    Create permutation.java
    Sample Input:
    Given Number: 143
    Sample Output:
    Permutations are:
    134
    143
    314
    341
    413
    431
    Test cases:
    1. 0
    2. 111
    3. 505
    4. -143
    5. -598

---
+++ permutation.java
@@ -0,0 +1,107 @@
+import java.util.*;
+
+public class Permutation
+
+{  
+
+public static void main(String args[])   
+
+{
+
+int number ;  
+
+System.out.println("Enter the number");
+
+Scanner s = new Scanner(System.in);
+
+number=s.nextInt();
+
+findPermutation(number);  
+
+}  
+
+static void findPermutation(int number)   
+
+{  
+
+int temp = number, count = 0;  
+
+while (temp > 0)   
+
+{     
+
+count++;  
+
+temp = temp / 10;  
+
+}   
+
+int[] num = new int[count];  
+
+while (number > 0)  
+
+{  
+
+num[count-- - 1] = number % 10;  
+
+number = number / 10;  
+
+}  
+
+while (findsNextpermutation(num))   
+
+{  
+
+
+
+for (int i = 0; i < num.length; i++)  
+
+System.out.print(num[i]);  
+
+System.out.print("\n");  
+
+}  
+
+}   
+
+static boolean findsNextpermutation(int[] p)   
+
+{  
+
+for (int a = p.length - 2; a >= 0; --a)  
+
+if (p[a] < p[a + 1])  
+
+for (int b = p.length - 1;; --b)  
+
+if (p[b] > p[a])   
+
+{    
+
+int t = p[a];  
+
+p[a] = p[b];  
+
+p[b] = t;  
+
+for (++a, b = p.length - 1; a < b; ++a, --b)  
+
+{  
+
+t = p[a];  
+
+p[a] = p[b];  
+
+p[b] = t;  
+
+}  
+
+return true;  
+
+}  
+
+return false;  
+
+}  
+
+} 
